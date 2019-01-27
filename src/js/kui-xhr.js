15db1a8222c722ddb54addd7dd4309244064b2ed
Christian Gann <guo.guo.gan@gmail.com>
2019-01-28 00:44:13 +0800

    added xhr to replace jquery ajax

---
+++ src/js/kui-xhr.js
@@ -0,0 +1,60 @@
+/**
+ * xhr will replace ajax which encapsulates jquery ajax in kui framework.
+ * 
+ * @since 2.0
+ * 
+ * @version 1.0.0 - Created on Jan 26, 2019.
+ */
+var xhr = {};
+
+/**
+ * @private
+ */
+xhr.request = function (opts, method) {
+
+  var url = opts.url;
+  var data = opts.data;
+  var type = opts.type || 'json';
+  var success = opts.success;
+  var error = opts.error;
+
+  var req  = new XMLHttpRequest();
+  req.open(method, url, true);
+  req.onload = function () {
+    var resp = req.responseText;
+    if (type == 'json') 
+      resp = JSON.parse(resp);
+    if (req.readyState == 4 && req.status == "200") {
+      if (success) success(resp);
+    } else {
+      if (error) error(resp);
+    }
+  }
+  if (data)
+    req.send(JSON.stringify(data));
+  else 
+    req.send(null);
+};
+
+/**
+ * @see xhr.request
+ */
+xhr.get = function (opts) {
+  xhr.request(opts, 'GET');
+};
+
+xhr.post = function (opts) {
+  xhr.request(opts, 'POST');
+};
+
+xhr.put = function (opts) {
+  xhr.request(opts, 'PUT');
+};
+
+xhr.delete = function (opts) {
+  xhr.request(opts, 'DELETE');
+};
+
+xhr.connect = function (opts) {
+  xhr.request(opts, 'CONNECT');
+};
